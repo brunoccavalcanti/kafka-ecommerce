0860a953493a2f7b9d9aaac5a91df2b80c302f15
Bruno Cavalcanti <brunoc.cavalcanti22@gmail.com>
2021-04-09 10:18:49 -0300

    Adde log feature

---
+++ src/main/java/br/edu/ecommerce/LogService.java
@@ -0,0 +1,40 @@
+package br.edu.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class LogService {
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties()) ;
+        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
+        while(true){
+            var records = consumer.poll(Duration.ofMillis(100));
+            if(!records.isEmpty()){
+                System.out.println("\n\nEncontrei " + records.count() + " registros");
+                for(var record : records){
+                    System.out.println("-----------------------------------------------");
+                    System.out.println("LOG " + record.topic());
+                    System.out.println("Key: " + record.key());
+                    System.out.println("Value: " + record.value());
+                    System.out.println("Partition: " + record.partition());
+                    System.out.println("Offset: " + record.offset());
+                }
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
+        return properties;
+    }
+}
