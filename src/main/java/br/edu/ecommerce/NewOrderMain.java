d4498a87d142cef7385f50b97a81cbcfc193d1a6
Bruno Cavalcanti <brunoc.cavalcanti22@gmail.com>
2021-04-09 10:52:17 -0300

    Added the ability to split messages from the same group to different partitions, managing to process several at the same time. The message key has also been changed to a unique and random id

--- src/main/java/br/edu/ecommerce/NewOrderMain.java
+++ src/main/java/br/edu/ecommerce/NewOrderMain.java
@@ -8,24 +8,28 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "13212,3252,152";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("Sucesso! enviando: " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        };
-        var email = "Thank you! We're processing your order!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, callback).get();
-        producer.send(emailRecord, callback).get();
+        for(int i = 0; i < 50; i++) {
+            var key = UUID.randomUUID().toString();
+            var value = key + ",3252,152";
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+            Callback callback = (data, ex) -> {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println("Sucesso! enviando: " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+            };
+            var email = "Thank you! We're processing your order!";
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+            producer.send(record, callback).get();
+            producer.send(emailRecord, callback).get();
+        }
     }
 
     private static Properties properties() {
